c39b554a04fcdeefea020f8a2223cfc495bce01f
isabel.llanes <isabel.llanes@thougthworks.com>
2022-10-17 15:19:36 +0200

    refacto actionCard

---
+++ src/action/ActionCard.js
@@ -0,0 +1,8 @@
+function ActionCard({action}){
+   
+    return(
+        <div>{action.temperature}</div>
+    )
+}
+
+export default ActionCard;
--- src/action/ActionList.js
+++ src/action/ActionList.js
@@ -1,16 +1,21 @@
 import { useEffect, useState } from "react";
+import ActionCard from "./ActionCard";
 
-function ActionList(){
-    useEffect(() => {
-fetch('http://localhost:8080/actions')
-.then(response => response.json())
-.then(data => setEvents(data))
-    }, []);
+function ActionList() {
+  useEffect(() => {
+    fetch("http://localhost:8080/actions")
+      .then((response) => response.json())
+      .then((data) => setActions(data));
+  }, []);
 
-    const [events, setEvents] = useState([]);
-return(
-<div>{JSON.stringify(events)}</div>
-)
+  const [actions, setActions] = useState([]);
+  return (
+    <div>
+      {actions.map((a) => (
+        <ActionCard action={a} />
+      ))}
+    </div>
+  );
 }
 
 export default ActionList;
