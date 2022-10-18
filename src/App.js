21f6be00af314bff70308042f99bf8546c29b9dd
isabel.llanes <isabel.llanes@thougthworks.com>
2022-10-18 11:08:37 +0200

    Fix import on main component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import ActionList from './action/ActionList';
-import SelectAction from './action/SelectAction';
-
 
 function App() {
   return (
