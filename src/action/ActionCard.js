45501e55b2d8c9e1aeebc93a972116048e2d585e
isabel.llanes <isabel.llanes@thougthworks.com>
2022-10-18 11:05:12 +0200

    Show rest of information on ActionCard

--- src/action/ActionCard.js
+++ src/action/ActionCard.js
@@ -1,7 +1,15 @@
+import './ActionCard.css'
+
 function ActionCard({action}){
    
     return(
-        <div>{action.temperature}</div>
+        
+        <div className='action-list_card'>
+            <div>Date: {action.timestamp.toString().substring(0, 9)}</div>
+            <div>Action: {action.type}</div>
+            <div>Humidity: {action.humidity}</div>
+            <div>Temperature: {action.temperature}</div>
+        </div>
     )
 }
 
