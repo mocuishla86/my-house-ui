22feb7defab2258e0e71cfa93bdda63dec6d585f
isabel.llanes <isabel.llanes@thougthworks.com>
2022-10-19 15:41:20 +0200

    fix date format

--- src/action/ActionCard.js
+++ src/action/ActionCard.js
@@ -1,11 +1,12 @@
 import './ActionCard.css'
+import dateFormat from "dateformat";
 
 function ActionCard({action}){
    
     return(
         
         <div className='action-list_card'>
-            <div>Date: {action.timestamp.toString().substring(0, 9)}</div>
+            <div>Date: {dateFormat(Date.parse(action.timestamp), "yyyy-mm-dd")}</div>
             <div>Action: {action.type}</div>
             <div>Humidity: {action.humidity}</div>
             <div>Temperature: {action.temperature}</div>
